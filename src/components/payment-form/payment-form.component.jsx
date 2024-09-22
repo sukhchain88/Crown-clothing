7442c6a8c0aa786397effad54a10f8bb13669b37
sukhchain88 <sukhchain1097@gmail.com>
2024-09-22 12:23:48 +0530

    Improve readability created payment-action-button component as well as payment-form-item component

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,83 +1,25 @@
-import React, { useState } from "react";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import React, { useState, Fragment } from "react";
 import Button from "../button/button.component";
-import { useContext } from "react";
-import { CartContext } from "../../context/cart.context";
-import { UserContext } from "../../context/user.context";
-import "./payment-form.scss";
+import PaymentFormItem from "../payment-form-item/payment-form-items.component";
+import "./payment-form.style.scss";
 
 const PaymentForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
-  const { cartTotal } = useContext(CartContext);
-  const { currentUser } = useContext(UserContext);
-  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+  const [isPaymentFormVisible, setIsPaymentFormVisible] = useState(false);
 
-  const paymentHandler = async (e) => {
-    e.preventDefault();
-
-    if (!stripe || !elements) {
-      return;
-    }
-
-    setIsProcessingPayment(true);
-    try {
-      const response = await fetch(
-        "/.netlify/functions/create-payment-intent",
-        {
-          method: "post",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ amount: cartTotal * 100 }),
-        }
-      ).then((res) => res.json());
-
-      console.log("response", response);
-      const { clientSecret } = response;
-
-      const cardElement = elements.getElement(CardElement);
-
-      const paymentResult = await stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-          card: cardElement,
-          billing_details: {
-            name: currentUser ? currentUser.displayName : "Guest",
-          },
-        },
-      });
-
-      setIsProcessingPayment(false);
-
-      if (paymentResult.error) {
-        alert(`Payment failed: ${paymentResult.error.message}`);
-      } else {
-        if (paymentResult.paymentIntent.status === "succeeded")
-          alert("Payment successful!");
-      }
-    } catch (err) {
-      console.error("Payment processing error:", err.message);
-      alert("An error occurred during the payment process. Please try again.");
-      setIsProcessingPayment(false);
-    }
-  };
+  const openPaymentForm = () => setIsPaymentFormVisible(true);
+  const closePaymentForm = () => setIsPaymentFormVisible(false);
 
   return (
-    <div className="payment-popup">
-      <div className="payment-container">
-        <form onSubmit={paymentHandler}>
-          <h2>Credit Card Payment:</h2>
-          <CardElement />
-          <Button
-            type="submit"
-            buttonType="inverted"
-            disabled={isProcessingPayment}
-          >
-            {isProcessingPayment ? "Processing..." : "Pay Now"}
-          </Button>
-        </form>
-      </div>
-    </div>
+    <Fragment>
+      <Button onClick={openPaymentForm}>Open Payment Form</Button>
+      {isPaymentFormVisible && (
+        <div className="payment-popup">
+          <div className="payment-container">
+            <PaymentFormItem closePaymentForm={closePaymentForm} />
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 };
 
